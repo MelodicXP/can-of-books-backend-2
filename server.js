854fe1a8ccf3d68b22d883a09477cac8654c7a86
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-11-28 06:47:28 -0600

    Establish proof of life for server

--- server.js
+++ server.js
@@ -1,43 +1,52 @@
 'use strict';
 
 require('dotenv').config();
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const mongoose = require('mongoose');
-const bookHandler = require('./Modules/bookHandler');
-const verifyUser = require('./Modules/authorize');
+
+// Todo - for now try to get from environment
+mongoose.connect('mongodb://127.0.0.1:27017/books');
+
+const PORT = process.env.PORT || 3002;
 
 const app = express();
-app.use(cors());
+app.use(cors()); // Middleware
 
-// Required to allow req body to show content (allows server to handle incoming JSON data from client)
-app.use(express.json());
-
-// PORT from .env file
-const PORT = process.env.PORT || 3001;
-
-mongoose.connect(process.env.MONGODB_CONN);
-
-const db = mongoose.connection;
-
-// MongoDB connection status (confirm if able to connect or not)
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', () => console.log('Mongoose is connected'));
-
-// Test connection to server
-app.get('/test', (request, response) => {
-
-  response.send('test request received');
-
+app.get('/', async (request, response) => {
+  response.send('Welcome to home page!');
 });
 
-// Verify user is authenticated before sending back any data
-app.use(verifyUser);
-
-// Retrieve data, post data, and delete data from database
-app.get('/books', bookHandler.getBooks);
-app.post('/books', bookHandler.postBooks);
-app.put('/books/:id', bookHandler.updateBooks);
-app.delete('/books/:id', bookHandler.deleteBooks);
-
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+// // Required to allow req body to show content (allows server to handle incoming JSON data from client)
+// app.use(express.json());
+
+// // PORT from .env file
+// const PORT = process.env.PORT || 3001;
+
+// mongoose.connect(process.env.MONGODB_CONN);
+
+// const db = mongoose.connection;
+
+// // MongoDB connection status (confirm if able to connect or not)
+// db.on('error', console.error.bind(console, 'connection error'));
+// db.once('open', () => console.log('Mongoose is connected'));
+
+// // Test connection to server
+// app.get('/test', (request, response) => {
+
+//   response.send('test request received');
+
+// });
+
+// // Verify user is authenticated before sending back any data
+// app.use(verifyUser);
+
+// // Retrieve data, post data, and delete data from database
+// app.get('/books', bookHandler.getBooks);
+// app.post('/books', bookHandler.postBooks);
+// app.put('/books/:id', bookHandler.updateBooks);
+// app.delete('/books/:id', bookHandler.deleteBooks);
+
+
